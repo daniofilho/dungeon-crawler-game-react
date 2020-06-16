5d98c35eaafd91cf5fdeabf525ed64f8a0c0f843
Dânio <danio.filho@navegarti.com.br>
2020-06-16 16:38:41 -0300

    refatorando componente Input

---
+++ client/src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import React, { InputHTMLAttributes } from 'react';
+import { DivInput } from './style';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  theme: string;
+};
+
+const Input: React.FC<InputProps> = ({ ...rest }) => {
+  return (
+    <DivInput>
+      <input {...rest} />
+    </DivInput>
+  );
+};
+
+export default Input;
